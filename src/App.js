6a345d71196cb04ebf8a6c6f54e93f037b2bff99
rafael.ramirez <rafa.rami12.10@gmail.com>
2022-07-04 23:03:14 -0500

    Feat:The API call was made and the structure was started

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
+import React, {useState } from 'react';
 import './App.css';
+import { Route,Link,BrowserRouter,Routes } from 'react-router-dom';
+import{Conexion} from './conexion/conexion';
+import { Cards } from './Cards/card';
+import Main from './Components/Main';
+import './Components/style.css';
 
-function App() {
+
+const Home = () => <h1>Rafa</h1>
+
+const Pokemons = () => {
+  const [url,setUrl]=useState ('https://pokeapi.co/api/v2/pokemon/');
+  const estado = Conexion(url);
+  const {cargando,dato}=estado
+  cargando? console.log('cargando'): console.log(dato.results);
   return (
+  <div>
+      <h1>Pokemones</h1>
+      {
+      cargando
+      ?
+      <h1>cargando</h1>
+      :
+      <div>
+      <Cards results={dato.results}/>
+
+      <div className='botones'>
+          <button className='boton' onClick={()=>setUrl(dato.previous)}>Anterior</button>
+          <button className='boton' onClick={()=>setUrl(dato.next)}>Siguiente</button>
+      </div>
+      </div>
+      }
+  </div>
+  )
+}
+
+  function Pokemon(){
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Pokemones</h1>
+    <Main />
+  </div>
+  )
+
+} 
+
+const Matricula = () =>{
+
+  return(
+    <div>
+      <h1 className='matricula'>190024</h1><br></br>
+      <p className='matricula'>Rafael Ramirez Ortega</p>
     </div>
-  );
+  )
+}
+
+
+const App = () => {
+  return(
+    <BrowserRouter>
+    <header>
+      <nav>
+      <Link to='/' className='nav-bar'>
+        Inicio
+      </Link>
+
+      <Link to='/Pokemons' className='nav-bar'>
+        Pokemons
+      </Link>
+
+      <Link to='/Pokemon' className='nav-bar'>
+        Pokemon
+      </Link>
+
+      <Link to='/Matricula' className='nav-bar'>
+        Matricula
+      </Link>
+      </nav>
+    </header>
+
+    <Routes> 
+    <Route exact path='/' element={<Home />} >
+    </Route>
+
+    <Route path='/Pokemons'  element={<Pokemons />}>
+    </Route>
+
+    <Route path='/Pokemon'  element={<Pokemon />}>
+    </Route>
+
+    <Route path='/Matricula'  element={<Matricula />}>
+    </Route> 
+    </Routes>
+    </BrowserRouter>
+    )
 }
 
 export default App;
